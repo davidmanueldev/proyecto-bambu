682f9f0da26e123d90b8bb88cb7613afba03bc68
David Manuel <davidmanuel.dev@gmail.com>
2024-09-14 19:45:13 -0400

    Estructura Básica

---
+++ src/components/Notification.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>Entrega gratuita para todos los pedidos ubicados cerca del Multicine en Rio Seco!
+    </div>
+  )
+}
+
+export default Notification
